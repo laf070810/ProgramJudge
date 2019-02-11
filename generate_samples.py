6a1b0cefccaeca46687fbb8dcffd919501fff69b
laf070810 <37955849+laf070810@users.noreply.github.com>
2019-02-11 15:43:21 +0800

    Update generators and judge
    New generators and new powerful judge.

--- generate_samples.py
+++ generate_samples.py
@@ -40,5 +40,94 @@ def generate_samples_week1ex4():
                            '---------------------------------------'])
 
 
+def generate_samples_week2ex1():
+    with open('input_1.txt', 'w') as input:
+        for i in range(50):
+            input.write('51\n')
+            input.write('50\n')
+            for j in range(50):
+                input.write(str(i + 1) + ' ' + str(j + 1) + '\n')
+
+    with open('output_1.txt', 'w') as output:
+        for i in range(50):
+            output.write(str((i + 1) * 50) + '\n')
+            for j in range(50):
+                for k in range(i + 1):
+                    output.write('*' * (2 * k + 1) + ' ' * (j + 1) + '*' * (2 * (i - k) + 1) + '\n')
+
+
+def generate_samples_week2ex2():
+    with open('input_2.txt', 'w') as input:
+        with open('output_2.txt', 'w') as output:
+            import random
+            for _ in range(100):
+                k = random.randint(1, 1000)
+                input.write(str(k + 1) + '\n')
+                input.write(str(k) + '\n')
+                output.write(str(k) + '\n')
+                for i in range(k):
+                    a = random.randint(-1000000000, 1000000000)
+                    b = random.randint(-1000000000, 1000000000)
+                    input.write(str(a) + ' ' + str(b) + '\n')
+                    output.write(str(b) + ' ' + str(a) + '\n')
+
+
+def generate_samples_week2ex3():
+    primes = []
+    for i in range(2, 1000000000):
+        if isPrime(i):
+            primes.append(i)
+        if i % 10000 == 0:
+            print(str(i) + ' ' + str(len(primes)))
+        if len(primes) > 2000:
+            break
+
+    import random
+    with open('input_3.txt', 'w') as input:
+        with open('output_3.txt', 'w') as output:
+            for i in range(10):
+                input.write('11' + '\n')
+                input.write('10' + '\n')
+                output.write('10' + '\n')
+                for j in range(10):
+                    a = random.randint(1, 2000)
+                    input.write(str(a) + '\n')
+                    output.write(str(primes[a - 1]) + '\n')
+
+
+def generate_samples_week2ex4():
+    import random
+
+    with open('input_4.txt', 'w') as input:
+        with open('output_4.txt', 'w') as output:
+            for i in range(10):
+                input.write('11' + '\n')
+                input.write('10' + '\n')
+                output.write('10' + '\n')
+                for j in range(10):
+                    a = random.randint(1, 10000)
+                    b = random.randint(1, 10000)
+                    gcd, lcm = gcdlcm(a, b)
+                    input.write(str(a) + ' ' + str(b) + '\n')
+                    output.write(str(gcd) + ' ' + str(lcm) + '\n')
+
+
+def isPrime(n):
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def gcdlcm(a, b):
+    a0, b0 = a, b
+    if (b > a):
+        a, b = b, a
+    while(a % b != 0):
+        a = a % b
+        a, b = b, a
+    return b, a0 * b0 // b
+
+
 if __name__ == '__main__':
-    generate_samples_week1ex4()
+    generate_samples_week2ex3()
--- judge.py
+++ judge.py
@@ -7,6 +7,12 @@ import time
 import zipfile
 import shutil
 from openpyxl import load_workbook
+from openpyxl.styles import Font, Color, colors
+
+WA = 0
+AC = 1
+TLE = 2
+RTE = 3
 
 
 def check_once(executable, input, output):
@@ -15,18 +21,26 @@ def check_once(executable, input, output):
     :param executable: String. the path of the executable
     :param input: String. sample input
     :param output: String. sample output
-    :return: True or False
+    :return: The constants, such as AC, TLE, etc.
     '''
-    ret = subprocess.run(
-        executable,
-        input=input,
-        stdout=subprocess.PIPE,
-        encoding='gbk')
+    try:
+        ret = subprocess.run(
+            executable,
+            input=input,
+            stdout=subprocess.PIPE,
+            timeout=1,
+            encoding='gbk')
+        ret.check_returncode()
+    except subprocess.TimeoutExpired:
+        return TLE
+    except subprocess.CalledProcessError:
+        return RTE
+
     if (ret.stdout.replace('\r\n', '\n').strip('\n') != output
             and ret.stdout.replace('\r\n', '\n').strip('\n') + '\n' != output):  # 忽略末尾换行
-        return False
+        return WA
     else:
-        return True
+        return AC
 
 
 def check(executable, inputs, outputs):
@@ -34,8 +48,8 @@ def check(executable, inputs, outputs):
 
     :param executable: String. the path of the executable
     :param inputs: list of String. sample input
-    :param outputs: list of String. sample Output
-    :return: list of True and False
+    :param outputs: list of String. sample output
+    :return: list of the constants
     '''
     return [check_once(executable, inputs[i], outputs[i]) for i in range(len(inputs))]
 
@@ -72,49 +86,103 @@ def get_samples(n):
 
     return inputs, outputs
 
+def extract_zip(file):
+    '''extract a zip file to current directory.
 
-WORKBOOK_PATH = '新生c.xlsx'
-wb = load_workbook(WORKBOOK_PATH)
-ws = wb.active
-cell_range = ws['A3':'A77']
-names = [cell[0] for cell in cell_range]
+    :param file: String, zip file name
+    :return: no return
+    '''
+    with zipfile.ZipFile(file, 'r') as zf:
+        for name in zf.namelist():
+            if not os.path.exists(name.encode('cp437').decode('gbk')):
+                os.renames(zf.extract(name), name.encode('cp437').decode('gbk'))
 
-files = os.listdir('.')
-for file in files:
-    if not os.path.isdir(file):
-        if zipfile.is_zipfile(file) and file.split('.')[1] == 'zip':
-            # ----------Extract the zip file----------
-            with zipfile.ZipFile(file, 'r') as zf:
-                for name in zf.namelist():
-                    if not os.path.exists(name.encode('cp437').decode('gbk')):
-                        os.renames(zf.extract(name), name.encode('cp437').decode('gbk'))
 
+def main_zips():
+    '''main program for zip files structure
+
+    :return: no return
+    '''
+    files = os.listdir('.')
+    zipfilenames = []
+    dirnames = []
+
+    # ----------Extract the zip files----------
+    for file in files:
+        if not os.path.isdir(file):
+            if zipfile.is_zipfile(file) and file.split('.')[1] == 'zip':
+                extract_zip(file)
+                dirnames.append(file.split('.')[0])
+                zipfilenames.append(file)
+
+    # ----------check for exercises----------
+    main_folders()
+
+    # ----------delete the extracted files and move the judged one to "Judged" dir----------
+    for dirname in dirnames:
+        shutil.rmtree(dirname)
+    if not os.path.exists('Judged'):
+        os.mkdir('Judged')
+    for zipfilename in zipfilenames:
+        shutil.move(zipfilename, 'Judged')
+
+
+def main_folders():
+    '''main program for folder structure
+
+    :return: no return
+    '''
+    WORKBOOK_PATH = '新生c.xlsx'
+    wb = load_workbook(WORKBOOK_PATH)
+    ws = wb['result']
+    cell_range = ws['A3':'A77']
+    names = [cell[0] for cell in cell_range]
+
+    files = os.listdir('.')
+    for file in files:
+        if os.path.isdir(file):
             # ----------check for exercises----------
-            dirname = file.split('.')[0]
-            participant_name = dirname.split(' ')[0]
-            for i in range(3, 4):
+            dirname = file
+            participant_name = dirname.split(' ')[0].split('+')[0]
+
+            for i in range(1, 5):
                 inputs, outputs = get_samples(i)
+                ws2 = wb['2_' + str(i)]
                 print('Judging ' + participant_name + ' for exercise ' + str(i))
+
                 begin_time = time.perf_counter()
                 result = check('./' + dirname + '/' + str(i) + '/' + str(i) + '.exe', inputs, outputs)
                 end_time = time.perf_counter()
-                accuracy = result.count(True) / len(result)
+
+                accuracy = result.count(AC) / len(result)
                 time_consumption = end_time - begin_time
                 print('Successfully judged ' + participant_name + ' for exercise ' + str(i))
                 print('accuracy\ttime')
                 print('{:.3f}'.format(accuracy) + '\t' + '{:.3f}'.format(time_consumption))
+
                 for name in names:
                     if name.value == participant_name:
                         ws.cell(row=name.row, column=name.col_idx + 3 * (i - 1) + 1).value = accuracy
                         ws.cell(row=name.row, column=name.col_idx + 3 * (i - 1) + 2).value = time_consumption
+                        for i in range(len(result)):
+                            if result[i] == AC:
+                                ws2.cell(row=name.row, column=name.col_idx + i + 1).value = 'AC'
+                                ws2.cell(row=name.row, column=name.col_idx + i + 1).font = Font(color=colors.GREEN)
+                            elif result[i] == WA:
+                                ws2.cell(row=name.row, column=name.col_idx + i + 1).value = 'WA'
+                                ws2.cell(row=name.row, column=name.col_idx + i + 1).font = Font(color=colors.RED)
+                            elif result[i] == TLE:
+                                ws2.cell(row=name.row, column=name.col_idx + i + 1).value = 'TLE'
+                                ws2.cell(row=name.row, column=name.col_idx + i + 1).font = Font(color=colors.DARKYELLOW)
+                            elif result[i] == RTE:
+                                ws2.cell(row=name.row, column=name.col_idx + i + 1).value = 'RTE'
+                                ws2.cell(row=name.row, column=name.col_idx + i + 1).font = Font(color=colors.DARKYELLOW)
                         wb.save(WORKBOOK_PATH)
                         print('Successfully saved ' + participant_name + ' for exercise ' + str(i) + '\n')
                         break
 
-            # ----------delete the extracted files and move the judged one to "Judged" dir----------
-            shutil.rmtree(dirname)
-            if not os.path.exists('Judged'):
-                os.mkdir('Judged')
-            shutil.move(file, 'Judged')
+    wb.close()
 
-wb.close()
+
+if __name__ == '__main__':
+    main_folders()
